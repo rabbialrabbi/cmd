ed016ce7acdb7522ec70dd8e115cb3bcccb70907
rabbi-dm4 <rabbialrabbi@gmail.com>
2019-12-08 11:41:07 +0600

    Starter

---
+++ cli_resource.js
@@ -0,0 +1,19 @@
+{
+    inux:{
+        'change_directory': 'cd ',
+            'create_directory': 'mkdir'
+    },
+    mysql:{
+        'setup':'sudo apt-get install mysql-server',
+            'configration': 'mysql_secure_installation',
+            'server'
+    },
+    phpstorm:{
+        sudo apt update
+        sudo apt install snapd
+        sudo snap install phpstorm --classic
+    },
+    git:{
+        'set-url':'git remote set-url origin https://<USERNAME>:<PASSWORD>@github.com/path/to/repo.git'
+    }
+}
