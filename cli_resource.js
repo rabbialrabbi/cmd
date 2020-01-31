f86493abe3fa52ae1e7352e55fead62acd31fecb
Rabbi-dm4 <rabbialrabbi@gmail.com>
2020-01-31 09:01:18 +0600

    Update

--- cli_resource.js
+++ cli_resource.js
@@ -6,7 +6,11 @@
     mysql:{
         'setup':'sudo apt-get install mysql-server',
             'configration': 'mysql_secure_installation',
-            'server'
+            'use mysql': ' sudo mysql -u username -p',
+            'show database': 'show databases;',
+            'create user': "GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' IDENTIFIED BY 'password';"
+        'create database': 'CREATE DATABASE tutorial_database;',
+            'delete all record in a table': 'TRUNCATE TABLE  table_name;'
     },
     phpstorm:{
         sudo apt update
@@ -14,6 +18,12 @@
         sudo snap install phpstorm --classic
     },
     git:{
-        'set-url':'git remote set-url origin https://<USERNAME>:<PASSWORD>@github.com/path/to/repo.git'
+        'set-url':'git remote add origin https://<USERNAME>:<PASSWORD>@github.com/path/to/repo.git'
+    },
+    phpserver:{
+        'Server_setup': 'sudo apt-get install apache2',
+            'php restart': 'sudo service php7.4-fpm restart',
+            'apache server restart': 'sudo service apache2 restart'
+
     }
 }
